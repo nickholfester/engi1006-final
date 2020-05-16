2e083c4dd3c043f3d720ec6cfe6e4589254b7e8d
Nicholas Holfester <nickholfester@gmail.com>
2020-05-16 00:26:11 -0400

    Completed weather page. Added images to static for comparison to generated plots. Finished.

--- NOAA_API.py
+++ NOAA_API.py
@@ -11,20 +11,21 @@ import requests
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# get NY stations
-def fetch_state_stations(base_url, locationid, token):
-    url = base_url+'stations'
+# get Nstate weather stations
+def fetch_state_stations(stat, locationid, token):
+    base_url = 'https://www.ncdc.noaa.gov/cdo-web/api/v2/stations'
     params = {'locationid': str(locationid)}
     
-    r = requests.get(url, params = params, headers = token)
+    r = requests.get(base_url, params = params, headers = token)
     
     data = r.json()['results']
     data_df = pd.DataFrame.from_dict(data)
     data_df.plot(x='longitude', y='latitude', kind='scatter', grid= True)
-    plt.savefig('static/ny_stations.png')
+    plt.savefig('static/'+str(stat)+'_stations.png')
 
     return data_df
 
+# used to sort stations by region - completed
 def group_stations_ny(station_data):
     ny_dict = {}
     
@@ -36,7 +37,7 @@ def group_stations_ny(station_data):
     
     return ny_dict
 
-# already include station, date, etc
+# used to retrieve temperature from stations - in progress
 def get_temp(ny_dict, params, token):
     base_url = 'https://www.ncdc.noaa.gov/cdo-web/api/v2/data'
     params['datatypeid'] = 'DPNP' #TAVG
@@ -62,53 +63,7 @@ def get_temp(ny_dict, params, token):
                 print(r)
                 temp_df = pd.DataFrame.from_dict(r.json())
             
-        return temp_df
-
-
-
-# token dictionary
-mytoken = "YoiLDWrDxUoZFonrlVYOrItiGgDiueBe"
-token ={'token':mytoken}
-
-# base url
-base_url = 'https://www.ncdc.noaa.gov/cdo-web/api/v2/'
-
-# base parameters
-datasetid = "GHCND"
-datatypeid = ""             # temperature data code
-limit = "1000"                  # amount of data max
-stationid = "GHCND:USW00023129" # choose specific station, could do multiple
-startdate = ""
-enddate = ""
-params = {'datasetid':datasetid, 'datatypeid':datatypeid, 'limit':limit, 'stationid':stationid,\
-          'startdate':startdate, 'enddate':enddate}
-
-# call functions
-station_data_ny = fetch_state_stations(base_url, 'FIPS:36', token)
-ny_dict = group_stations_ny(station_data_ny)
-
-## testing temp get
-j = 1
-region_ids = ny_dict['nyc']['id']
-params['stationid'] = str(region_ids.iloc[j])
-params['startdate'] = '2012-01-01'
-params['enddate'] = '2012-12-31'
-params['datatypeid'] = 'TAVG'
-
-url = base_url+'data'
-
-r = requests.get(url, params = params, headers = token)
-print(r)
-data = r.json()
-
-
-    
-    
-    
-
-
-
-
+            return temp_df
 
 
 
--- app.py
+++ app.py
@@ -11,18 +11,37 @@ Created on Tue Apr 21 14:57:17 2020
 
 #import statements
 from flask import Flask, render_template
+from NOAA_API import fetch_state_stations
 import pandas as pd
 
 #Flask app variable
 app = Flask(__name__)
 
+# generate images of current weather station distributions for static
+
+# token dictionary
+def makestations():
+    # token and location IDs from NOAA website - generates each run so data is updated
+    mytoken = "YoiLDWrDxUoZFonrlVYOrItiGgDiueBe"
+    token ={'token':mytoken}
+    
+    fetch_state_stations('ny', "FIPS:36", token)
+    fetch_state_stations('ca', "FIPS:06", token)
+    fetch_state_stations('il', "FIPS:17", token)
+
 
 # making class data-frame for /classes
-df = pd.DataFrame({'Class': ['Accelerated Physics II', 'Intro to PDE', 'Fourier Analysis', \
-                             'Intro to Comp.', 'Physics of Atmo/Ocean'],
-                   'Department': ['Physics', 'Applied Math', 'Math', 'Engineering', 'Environ.'],
-                   'Course ID': ['PHYS UN2802', 'APMA E3102', 'MATH GU4032', 'ENGI E1006', 'EESC GU4930'],
-                   'Professor': ['Cole', 'Tippett', 'Woit', 'Paine', 'Gordon']})
+def makeclasses():
+    df = pd.DataFrame({'Class': ['Accelerated Physics II', 'Intro to PDE', 'Fourier Analysis', \
+                                 'Intro to Comp.', 'Physics of Atmo/Ocean'],
+                       'Department': ['Physics', 'Applied Math', 'Math', 'Engineering', 'Environ.'],
+                       'Course ID': ['PHYS UN2802', 'APMA E3102', 'MATH GU4032', 'ENGI E1006', 'EESC GU4930'],
+                       'Professor': ['Cole', 'Tippett', 'Woit', 'Paine', 'Gordon']})
+    return df
+    
+# run functions
+makestations()
+df = makeclasses()
 
 #static route
 @app.route("/")
